89a862f1498d8c69c9fa2f16431eb56e4773f0aa
Aryan <aryansep018@gmail.com>
2023-09-12 10:33:21 -0400

    fix: added chat box UI

--- messenger_app/src/App.js
+++ messenger_app/src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import LoginRegister from "./pages/LoginRegister";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <LoginRegister />;
+  return <HomePage />;
 }
 
 export default App;
